d163c570bfa01a2fbe0e88403e503ed91bd3928e
Callum Locke <callum.locke@thetrainline.com>
2019-09-03 13:22:21 +0100

    Upgrade red deps

--- .eslintrc.js
+++ .eslintrc.js
@@ -46,5 +46,12 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': 'off',
       },
     },
+
+    {
+      files: ['**/*.js'],
+      rules: {
+        '@typescript-eslint/explicit-function-return-type': 'off',
+      },
+    },
   ],
 }
